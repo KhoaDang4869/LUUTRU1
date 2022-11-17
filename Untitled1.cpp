e535a2e34121200e2db602721aa5e290bd4c3725
KhoaDang4869 <116790936+KhoaDang4869@users.noreply.github.com>
2022-11-17 15:57:45 +0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,53 @@
+#include"stdio.h"
+#include"stdlib.h"
+int nhap2c(int *a,int n, int m);
+int xuat2c(int *a, int n, int m);
+int sx(int *a, int n, int m);
+int main()
+{ int *a, n, m;
+  a=(int*)calloc(25,sizeof(int));
+  printf("\n nhap n"); scanf("%d", &n);
+  printf("\n mhap m"); scanf("%d",&m);
+  nhap2c(a,n,m);
+  xuat2c(a,n,m);
+  printf("*******\n");
+  sx(a,n,m);
+  printf("+++++\n");
+  xuat2c(a,n,m);
+}
+int nhap2c(int *a,int n, int m)
+{
+	for(int i =0;i<n;i++)
+	{ for(int j=0; j<m;j++)
+	  {
+	  	printf("\n Nhap phan tu thu a[%d][%d]:\t",i,j);
+	  	scanf("%d",(a+i*n+j));
+	}
+   }
+}
+int xuat2c(int *a, int n, int m)
+{  for(int i=0;i<n;i++)
+   {
+   	 for(int j=0;j<m;j++)
+   	 {
+   	 	printf("\t%d",*(a+i*n+j));
+   	 }
+     printf("\n");
+   }
+}
+   
+
+int sx(int *a,int n, int m)
+{ int tam, tich=m*n;
+for(int i=0;i<tich-1;i++)
+ for(int j=i+1;j<tich;j++)
+ {
+ 	if(*(a+i)>*(a+j))
+ 	{
+ 		tam=*(a+i);
+ 		*(a+i)=*(a+j);
+ 		*(a+j)=tam;
+ 	}
+ }
+ return 0;
+}
